e759ce0015ee037d14f3cb6fcae8f48e7c5e77db
Pratik Arote <pratik.a@mobinexttech.com>
2020-06-04 18:31:20 +0530

    Add project files.

--- Controllers/CreateFileController.cs
+++ Controllers/CreateFileController.cs
@@ -2,6 +2,7 @@
 using File_Transfer_System.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,15 +16,38 @@ namespace File_Transfer_System.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            return View("CreateFile");
+            fileDetailsDAL = new FileDetailsDAL();
+            FileInfoModel fileInfoModel = new FileInfoModel();
+            fileInfoModel = fileDetailsDAL.GetFileInfo();
+            return View("CreateFile", fileInfoModel);
         }
 
         [HttpPost]
-        public JsonResult SaveFileDetails(FormCollection formData)
+        public JsonResult SaveFileDetails(FormCollection formData )
         {
             fileDetailsDAL = new FileDetailsDAL();
+            FileInfoModel fileinfo = new FileInfoModel();
+           
             var result = fileDetailsDAL.SaveFileDetails(formData);
-            return Json(result);
+            var files = Request.Files.GetMultiple("fileControl").ToList();
+            foreach (HttpPostedFileBase file in files)
+            {
+                //Checking file is available to save.  
+                if (file != null)
+                {
+                    var InputFileName = Path.GetFileName(file.FileName);
+                    var ServerSavePath = Path.Combine(Server.MapPath("~/UploadedFiles/") + InputFileName);
+                    //Save file to server folder  
+                    file.SaveAs(ServerSavePath);
+                    //assigning file uploaded status to ViewBag for showing message to user.  
+                    //ViewBag.UploadStatus += file.FileName + " files uploaded successfully.";
+                    ViewBag.UploadStatus += string.Format("<b>{0}</b> uploaded.<br />", InputFileName);
+                }
+               
+            }
+
+            var output = fileDetailsDAL.SaveAttachmentDetails(formData,files);
+            return Json(output);
         }
     }
 }
